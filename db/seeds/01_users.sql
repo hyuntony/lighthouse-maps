1d1f435b89632088c511539db09e7629b6bada4f
Andres Fernandez-Murray <andres.fmurray@gmail.com>
2021-06-27 17:34:16 +0000

    schema references

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,7 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (id, name, email, password)
+VALUES
+('1', 'Anthony', 'anthony@gmail.com', '123'),
+('2', 'Andy', 'andy@gmail.com', '123'),
+('3', 'Eliza', 'eliza@gmail.com', '123'),
+('4', 'Kevin', 'kevin@gmail.com', '123');
