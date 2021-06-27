15889851536b026834d1a1568283be07bda9e347
Andres Fernandez-Murray <andres.fmurray@gmail.com>
2021-06-27 15:03:03 +0000

    conflict

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -4,4 +4,16 @@ DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL
+  email VARCHAR(255) NOT NULL
+
 );
+
+
+
+
+
+id
+name
+email
+pw
+profile_picture_url
