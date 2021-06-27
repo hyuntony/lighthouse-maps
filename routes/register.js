3c15b2b594e2a2ce53919436c2b6bb026a69907b
hyuntony <ankim90@gmail.com>
2021-06-27 14:21:45 -0400

    add function to register button to insert data into users table with newUser AND install bcrypt

--- routes/register.js
+++ routes/register.js
@@ -6,6 +6,7 @@
  */
 
 const express = require('express');
+const { generateRandomString } = require('../helpers/helperfunc');
 const router  = express.Router();
 
 module.exports = (db) => {
@@ -13,7 +14,6 @@ module.exports = (db) => {
     db.query(`SELECT * FROM users;`)
       .then(data => {
         const users = data.rows;
-        console.log(users);
         res.render('urls_register');
       })
       .catch(err => {
@@ -22,8 +22,24 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
-  // router.post("/", (req, res) => {
-  //   db.query(``)
-  // })
+  router.post("/", (req, res) => {
+    const newUser = req.body;
+    const randomId = generateRandomString();
+    if (newUser.email === "" | newUser.password === "" | newUser.name === "") {
+      return res.status(400).send("Email, name, or password is entered empty");
+    }
+    db.query(`SELECT email FROM users
+              WHERE email = $1`, [newUser.email])
+      .then(data => {
+        if (data.rows.length > 0) {
+          return res.status(400).send("Registered user with that email address already exists");
+        } else {
+          db.query(`INSERT INTO users (id, name, email, password)
+                    VALUES ('${randomId}', $1, $2, $3)
+                    RETURNING *`, [newUser.name, newUser.email, newUser.password])
+            .then(res.render('index'));
+        }
+      });
+  });
   return router;
 };
