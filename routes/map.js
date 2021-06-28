f44269a43cf599ed9e5ce242a35e8a647c08fb44
hyuntony <ankim90@gmail.com>
2021-06-28 19:55:25 -0400

    make create new map ejs with a form to be filled

--- routes/map.js
+++ routes/map.js
@@ -2,16 +2,30 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
+  router.get("/new", (req, res) => {
+    const userID = req.session.user_id;
+    db.query(`SELECT * FROM users
+            WHERE id = $1`, [userID])
+      .then(data => {
+        let user = '';
+        if (data.rows.length > 0) {
+          user = data.rows[0].name;
+        }
+        const templateVars = { user };
+        return res.render("new_map", templateVars);
+      });
+  });
+
   router.get("/:map", (req, res) => {
 
     const mapID = req.params.map;
 
     db.query(`SELECT * FROM maps WHERE id = $1 `, [mapID])
-    .then((data => {
-      const map = data.rows[0];
-      const templateVars = {map};
-      res.render('map', templateVars);
-    }))
+      .then((data => {
+        const map = data.rows[0];
+        const templateVars = {map};
+        res.render('map', templateVars);
+      }));
   });
 
 
