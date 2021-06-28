141254c9edb87f01af3d95eb25711c62baa7d28e
Andres Fernandez-Murray <andres.fmurray@gmail.com>
2021-06-28 14:05:52 +0000

    seed mock data

---
+++ db/seeds/04_favorites.sql
@@ -0,0 +1,6 @@
+INSERT INTO favorites (id, users_id, maps_id)
+VALUES
+(1,'2',1),
+(2,'3',1),
+(3,'4',1);
+
---
+++ db/seeds/05_comments.sql
@@ -0,0 +1,6 @@
+INSERT INTO comments (id, users_id, maps_id, text, date_created)
+VALUES
+(1,'2',1,'amazing map!', NOW()),
+(2,'3',1,'first!', NOW()),
+(3,'4',1,'nice', NOW()),
+(4,'1',1,'Thanks!', NOW());
