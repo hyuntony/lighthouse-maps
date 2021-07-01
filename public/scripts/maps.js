d2da1605205e690bd4ca1b7a8397244b6f7b077a
hyuntony <ankim90@gmail.com>
2021-07-01 16:01:15 -0400

    maps css

--- public/scripts/maps.js
+++ public/scripts/maps.js
@@ -4,42 +4,13 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$(document).ready(()=> {
+$(() => {
 
+  $(".totalMapBox").height($(".totalMapBox").width());
 
-  const loadMaps = () => {
-    $.ajax({
-      method: 'GET',
-      url: 'http://localhost:8080/api/maps'
-    })
-      .done((res)=> {
-        renderMaps(res);
-      })
-      .fail((err) => console.log(`fail to get`, err));
-  };
-  // async call for maps
-  loadMaps();
-  // generate html articles from tweets
-  const createMapElement = map => {
-    // sanitize user input
-    // input like ${escape(map.thumbnail_url)}
+  const time = $("#time").val();
+  const adjustedTime = timeago.format(time);
 
-    const template =
-    `<div class='mapArticle'>
-      <div class = 'myMap'> </div>
-      <div class = myMapDetails
-    </div>`;
-
-    return template;
-  };
-
-  const renderMaps = arrayOfMaps => {
-            //clear current generated articles to prevent duplicates
-    $('#mapContainer').empty();
-    arrayOfMaps.forEach(map => {
-      const $map = createMapElement(map);
-      $('#mapContainer').prepend($map);
-    });
-  };
+  $()
 
 });
